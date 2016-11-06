9817b795b4c1272e4cbdebd47c913a2a0db18097
Christian Gimber <cgimber@ucla.edu>
2016-11-06 15:01:01 -0800

    push readings into a filtered data array

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 /* globals
 ---------------------------------------------------------------------*/
-var data;
+var readings;
+var data = [];
 
 /* document ready
 ---------------------------------------------------------------------*/
@@ -9,7 +10,7 @@ $(document).ready(function() {
     var proxy = "https://crossorigin.me/";
     var path = "http://tidesandcurrents.noaa.gov/api/datagetter?";
     var params = {
-        "range": "24",
+        "range": "72",
         "station": "9410230",
         "product": "water_temperature",
         "datum": "MLLW",
@@ -24,20 +25,69 @@ $(document).ready(function() {
     console.log(url);
 
     $.get(url, function(response) {
-            // console.log(response);
-            data = $.parseJSON(response).data;
-            console.log(data);
+            readings = $.parseJSON(response).data;
+            console.log(readings);
+
+            var currDate = getDate(readings[0].t);
+            var total = 0;
+            var temps = [];
+            var average = 0;
+
+            for (var i = 0; i < readings.length; i++) {
+                if (getDate(readings[i].t) == currDate) { // if this reading is for the current date,
+                    // add this temperature to the running total and store this reading
+                    var temp = parseFloat(readings[i].v);
+                    if (!isNaN(temp)) {
+                        total += temp;
+                        temps.push(readings[i]);
+                    }
+
+                    if (i == readings.length - 1) { // if this is the last reading,
+                        // calc avg temp
+                        average = total / temps.length;
+
+                        // add to data
+                        data.push({ date: currDate, temperature: average, readings: temps });
+                    }
+                } else {
+                    // calc avg temp
+                    average = total / temps.length;
+
+                    // add to data
+                    data.push({ date: currDate, temperature: average, readings: temps });
+
+                    // move to the next date and clear old values
+                    currDate = getDate(readings[i].t);
+                    total = 0;
+                    temps = [];
+
+                    // add this temperature to the running total and store this reading
+                    var temp = parseFloat(readings[i].v);
+                    if (!isNaN(temp)) {
+                        total += temp;
+                        temps.push(readings[i]);
+                    }
+                }
+            }
         })
         .done(function() {
             console.log("done");
+            console.log(data);
         })
         .fail(function() {
-            console.log("error");
+            console.error("error");
         })
         .always(function() {
             console.log("finished");
         });
+
 });
 
 /* functions
 ---------------------------------------------------------------------*/
+
+function getDate(t) {
+    // return the date of timestamp t
+    // e.g. "2016-11-02"
+    return t.substr(0, 10);
+}
