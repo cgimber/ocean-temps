9f7b70e326e126a74bfbed527588e97a69afd57d
Christian Gimber <cgimber@ucla.edu>
2016-11-07 01:19:12 -0800

    fill page with color-coded elements and define basic mobile styles

--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
+/*
+TODOS
+    -show/hide date and values on day hover event
+    -more granular view (e.g. just the readings for one day)
+    -tweak mobile styling
+    -station picker?
+    -date/range picker?
+*/
+
 /* globals
 ---------------------------------------------------------------------*/
 var readings;
 var data = [];
+var CONSTANTS = { "max_temp": 75, "min_temp": 50 };
+CONSTANTS.range = Math.abs(CONSTANTS.max_temp - CONSTANTS.min_temp);
 
 /* document ready
 ---------------------------------------------------------------------*/
@@ -11,7 +22,7 @@ $(document).ready(function() {
     var url = "http://tidesandcurrents.noaa.gov/api/datagetter";
     var params = {
         "csurl": url,
-        "range": "72",
+        "range": "2016",
         "station": "9410230",
         "product": "water_temperature",
         "datum": "MLLW",
@@ -23,7 +34,7 @@ $(document).ready(function() {
 
     $.get(proxy, params, function(response) {
             readings = $.parseJSON(response).data;
-            console.log(readings);
+            // console.log(readings);
 
             var currDate = getDate(readings[0].t);
             var total = 0;
@@ -70,6 +81,7 @@ $(document).ready(function() {
         .done(function() {
             console.log("done");
             console.log(data);
+            updateHTML(data);
         })
         .fail(function() {
             console.error("error");
@@ -83,8 +95,67 @@ $(document).ready(function() {
 /* functions
 ---------------------------------------------------------------------*/
 
+function updateHTML(_data) {
+    var days = "";
+    for (var i = _data.length - 1; i >= 0; i--) {
+        var date = "<div class='day__date'>" + formatDate(_data[i].date) + "</div>";
+        var value = "<div class='day__value'>" + Math.round(_data[i].temperature) + "&#176;</div>";
+        var day = "<div class='calendar__day' style='background:" + tempToRgba(_data[i].temperature, 0.75) + "'>" + value + "</div>";
+        days += day;
+    }
+    $('.calendar').append(days);
+}
+
 function getDate(t) {
     // return the date of timestamp t
     // e.g. "2016-11-02"
     return t.substr(0, 10);
+}
+
+function formatDate(d) {
+    var day = d.split('-')[1];
+    var month = d.split('-')[2];
+    var year = d.split('-')[0];
+    return day + "-" + month + "-" + year;
+}
+
+function tempToRgba(t, a) {
+    var r, b, g;
+    if (Math.abs(CONSTANTS.max_temp - t) < Math.abs(CONSTANTS.min_temp - t)) { // hotter than avg
+        r = Math.round(map(Math.abs(CONSTANTS.max_temp - t), CONSTANTS.range / 2, 0, 0, 255));
+        b = 0;
+        g = 255 - r;
+    } else {
+        r = 0;
+        b = Math.round(map(Math.abs(CONSTANTS.min_temp - t), CONSTANTS.range / 2, 0, 0, 255));
+        g = 255 - b;
+    }
+    return "rgba(" + r + ", " + g + ", " + b + ", " + a + ")";
+}
+
+function tempToHex(t) {
+    var r, b, g;
+    if (Math.abs(CONSTANTS.max_temp - t) < Math.abs(CONSTANTS.min_temp - t)) { // hotter than avg
+        r = Math.round(map(Math.abs(CONSTANTS.max_temp - t), CONSTANTS.range / 2, 0, 0, 255));
+        b = 0;
+        g = 255 - r;
+    } else {
+        r = 0;
+        b = Math.round(map(Math.abs(CONSTANTS.min_temp - t), CONSTANTS.range / 2, 0, 0, 255));
+        g = 255 - b;
+    }
+    return rgbToHex(r, g, b);
+}
+
+function map(value, start1, stop1, start2, stop2) {
+    return start2 + (stop2 - start2) * ((value - start1) / (stop1 - start1));
+}
+
+function rgbToHex(r, g, b) {
+    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
+function componentToHex(c) {
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
 }
